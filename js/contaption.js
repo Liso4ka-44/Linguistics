5d2008d101d1a4906046eabd18f343aa2e8a6c41
Syamon <s.truskalo@yandex.ru>
2022-01-17 23:11:02 +0300

    Изменил все как надо

--- js/contaption.js
+++ js/contaption.js
@@ -1,16 +1,21 @@
 $(document).ready(function() {
-    function slowScroll(id) {
-        var offset = 0;
-        $('html, body').animate({
-            scrollTop: $(id).offset().top + offset
-        }, 1000);
-        return false;
-    }
     $('#contaption__link').click(function() {
         // event.preventDefault();
         $('.contaption__spaeks__list').toggleClass("contaption__spaeks__list__full");
-        $('#contaption__link').attr("href", "#block1");
 
+        if ($(".contaption__spaeks__list").hasClass("contaption__spaeks__list__full")) {
+            $('#contaption__link').attr("href", "#contaption__link");
+            let elementClick = $(this).attr("href");
+            let destination = $(elementClick).offset().top;
+            $('html, body').animate({ scrollTop: destination }, 1500);
+            return false;
+        } else {
+            $('#contaption__link').attr("href", "#block1");
+            let elementClick1 = $(this).attr("href");
+            let destination1 = $(elementClick1).offset().top;
+            $('html, body').animate({ scrollTop: destination1 }, 1500);
+            return false;
+        }
     });
 
 
