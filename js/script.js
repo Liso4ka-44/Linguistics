445c52f992c8134c74d633c97eb64711f21442cf
Syamon <s.truskalo@yandex.ru>
2022-01-31 03:02:53 +0300

    пока что так

---
+++ js/script.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+    $('.header__link').click(function() {
+        $(".header__link").removeClass('active');
+        let dateNav = $(this).attr("date-nav");
+        $(`.header__link[date-nav="${dateNav}"]`).addClass("active");
+    });
+    let myEditor
+    ClassicEditor.create(document.querySelector('#editor')).then(editor => {
+        myEditor = editor
+    });
+    ClassicEditor.create(document.querySelector('#editor1')).then(editor => {
+        myEditor = editor
+    });
+    ClassicEditor.create(document.querySelector('#editor2')).then(editor => {
+        myEditor = editor
+    });
+    ClassicEditor.create(document.querySelector('#editor3')).then(editor => {
+        myEditor = editor
+    }).catch(error => {
+        console.error(error);
+    });
+});
