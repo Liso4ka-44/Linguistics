c243a88b661e9cb6ac4a3089f3131d7f582c8910
Syamon <s.truskalo@yandex.ru>
2022-02-10 22:16:08 +0300

    d

--- php/userOnl.php
+++ php/userOnl.php
@@ -1,13 +1,12 @@
 <?php
 include "connect.php";
 if (isset($_POST["login"])) {
-$mass = "";
-$login_bd = mysqli_query($connect, "SELECT * FROM `users` WHERE `login` = " . $_POST["login"]);
-$login_bd = mysqli_fetch_assoc($login_bd);
-if (empty($login_bd)) {
-$mass = "done";
-} else {
-$mass = "undone";
-}
-echo $mass;
+    $login_bd = mysqli_query($connect, "SELECT * FROM `users` WHERE `login` = " . $_POST["login"]);
+    $login_bd = mysqli_fetch_assoc($login_bd);
+    if (empty($login_bd)) {
+        $mass = "done";
+    } else {
+        $mass = "undone";
+    }
+    echo $mass;
 }
