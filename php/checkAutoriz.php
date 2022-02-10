f47f0f61a2a49c8627d57bbda0700796fc121b10
Syamon <s.truskalo@yandex.ru>
2022-02-10 23:25:15 +0300

    ddd

--- php/checkAutoriz.php
+++ php/checkAutoriz.php
@@ -1,12 +1,15 @@
 <?php
 include "connect.php";
-if ($_POST["login1"]) {
+if ($_POST["pass"]) {
     $autoriz = mysqli_query($connect, "SELECT * FROM `users` WHERE `login` = '" . $_POST["login1"] . "'");
     $row = mysqli_fetch_assoc($autoriz);
-    $pass = $_POST["pass"];
-    if (password_verify($pass, $row['password'])) {
+    $pass = (string) $_POST['pass'];
+    $hash = '$2y$07$BCryptRequires22Chrcte/VlQH0piJtjXl.0t1XkA8pw9dMXTpOq';
+    $hash1 = $row["password"];
+    if (password_verify($_POST["pass"], $hash1)) {
         echo 'Пароль правильный!';
     } else {
-        echo 'Пароль неправильный.';
+        echo $hash1;
     }
 }
+//rasmuslerdorf
--- php/userOnl.php
+++ php/userOnl.php
@@ -1,12 +1,11 @@
 <?php
 include "connect.php";
 if (isset($_POST["login"])) {
-    $login_bd = mysqli_query($connect, "SELECT * FROM `users` WHERE `login` = " . $_POST["login"]);
+    $login_bd = mysqli_query($connect, "SELECT * FROM `users` WHERE `login` = '" . $_POST["login"] . "'");
     $login_bd = mysqli_fetch_assoc($login_bd);
     if (empty($login_bd)) {
-        $mass = "done";
+        echo "done";
     } else {
-        $mass = "undone";
+        echo "undone";
     }
-    echo $mass;
 }
