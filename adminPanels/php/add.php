c49976ebc899cffd26589b275496a4aa032bd724
Syamon <s.truskalo@yandex.ru>
2022-03-01 23:25:02 +0300

    па

--- adminPanels/php/add.php
+++ adminPanels/php/add.php
@@ -42,7 +42,7 @@
                         </div>
                         <div class="editor__list">
                             <div class="editor1">
-                                <h4 class="ruText">Информация о конференции</h4><textarea class="edit" id="editor0"></textarea>
+                                <h4 class="ruText">Информация о конференции</h4><textarea class="edit " id="editor0"></textarea>
                             </div>
                             <div class="editor2">
                                 <h4 class="enText">Conference information</h4><textarea class="edit" id="editor1"></textarea>
