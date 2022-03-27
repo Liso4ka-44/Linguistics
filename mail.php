7ca5fbac06a3e720b2efbd89bef16d19c151b6e6
Syamon <s.truskalo@yandex.ru>
2022-03-27 22:56:35 +0300

    ввв

--- mail.php
+++ mail.php
@@ -2,15 +2,17 @@
 // Подключаем библиотеку PHPMailer
 use phpmailer\PHPMailer\PHPMailer;
 use phpmailer\PHPMailer\SMTP;
+use PHPMailer\src\Exception;
+
 require 'phpmailer/src/PHPMailer.php';
 require 'phpmailer/src/SMTP.php';
-
+require 'PHPMailer/src/Exception.php';
 // Создаем письмо
 $mail = new PHPMailer();
 $mail->CharSet = 'UTF-8';
-$mail -> setLanguage('ru','/phpmailer/language');
-$yourEmail = 's.truskalo@yandex.ru'; // ваш email на яндексе
-$password = 'aen,jk26repz'; // ваш пароль к яндексу или пароль приложения
+$mail->setLanguage('ru', '/phpmailer/language');
+$yourEmail = 'liza-volokhova@yandex.ru'; // ваш email на яндексе
+$password = '4955912721'; // ваш пароль к яндексу или пароль приложения
 
 $name = $_POST["name"];
 $email = $_POST["email"];
@@ -20,7 +22,7 @@ $mail->Mailer = 'smtp';
 $mail->Host = 'ssl://smtp.yandex.ru';
 $mail->Port = 465;
 $mail->SMTPAuth = true;
-$mail->Username = $yourEmail; // ваш email - тот же что и в поле From:
+$mail->Username = 'Елизавета В.'; // ваш email - тот же что и в поле From:
 $mail->Password = $password; // ваш пароль;
 
 // формируем письмо
@@ -29,14 +31,14 @@ $mail->Password = $password; // ваш пароль;
 $mail->setFrom($yourEmail, $name);
 
 // кому - получатель письма
-$mail->addAddress($yourEmail, 'Семён Трускало'); // кому
+$mail->addAddress($yourEmail, 'Елизавета Волохова'); // кому
 
 $mail->Subject = 'Проверка'; // тема письма
 
 $mail->msgHTML("<html><body>
 <h1>Письмо с сайта</h1>
-<p>".$text."</p>
-<p> Email отправителя ".$email."</p>
+<p>" . $text . "</p>
+<p> Email отправителя " . $email . "</p>
 </html></body>");
 
 if ($mail->send()) { // отправляем письмо
@@ -44,5 +46,4 @@ if ($mail->send()) { // отправляем письмо
 } else {
   echo 'Ошибка: ' . $mail->ErrorInfo;
 }
-exit("<meta http-equiv='refresh' content='0; url= /index.php'>"); 
-?>
+exit("<meta http-equiv='refresh' content='0; url= /index.php'>");
