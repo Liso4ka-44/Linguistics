6972f07a18dcdb2e465ea76ae3a0cb0356f5e582
Syamon <s.truskalo@yandex.ru>
2022-04-19 21:21:01 +0300

    меню поправил

--- connect.php
+++ connect.php
@@ -1,5 +1,5 @@
-<?php 
+<?php
 //  session_start();   
- $connect = mysqli_connect("localhost","root","","vkr");
-  //  $connect = mysqli_connect("localhost","linguistics","fKI0xHbILCDUWa","host__linguistics_mgimo_ru__00");
-    mysqli_set_charset($connect, 'utf8');
+$connect = mysqli_connect("localhost", "root", "", "mgimonew");
+//  $connect = mysqli_connect("localhost","linguistics","fKI0xHbILCDUWa","host__linguistics_mgimo_ru__00");
+mysqli_set_charset($connect, 'utf8');
